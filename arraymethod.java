557e9a22e5562f59aeaf8a532a700e58a29ce16f
GauravTendulkar26 <gauravtendulkar1165@gmail.com>
2024-01-17 00:28:56 +0530

    add array method

---
+++ arraymethod.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class arraymethod {
+    // tostring , aslist , deeptostring ; 
+    public static void main(String args[]) {
+        int a[] = {0,10,20,30} ;
+        String b[] = {"learn", "theday", "fine", "howareyou"};
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.asList(b));
+    }
+}
